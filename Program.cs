9e546e44baf770fdd2a4372da21f6d4749be2b5f
OVGI <girichevao@gmail.com>
2022-12-20 00:23:17 +0300

    add task_66

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30              */
+
+int m = 4, n = 8;
+Console.Write($"{Summ(m, n)}");
+
+int Summ(int left, int right)
+{
+    if (left >= right)
+        return left;
+    return left + Summ(left + 1, right);
+}
+
